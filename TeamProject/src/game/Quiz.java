ef842f29898219bc63f2c1d71dd89a151462f5fb
LG <LG@DESKTOP-C576NK8>
2023-12-03 14:56:43 +0900

    231203 퀴즈 게임 수정

--- TeamProject/src/game/Quiz.java
+++ TeamProject/src/game/Quiz.java
@@ -4,10 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 public class Quiz {
 
