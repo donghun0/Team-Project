132b8f902c778e10911543e0aa516b1d61e17245
LG <LG@10.50.9.78>
2023-12-06 22:53:23 +0900

    최윤하

--- TeamProject/src/game/GameStart.java
+++ TeamProject/src/game/GameStart.java
@@ -6,17 +6,24 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GameStart {
-	
-	private static int money = 0; // 돈 축적을 위한 변수
+    private static int money = 0; // 돈 축적을 위한 변수
     private static JLabel moneyLabel;
+    private static JFrame frame; // 게임 선택 창을 위한 프레임 변수
 
     public static void main(String[] args) {
+        // 이미 열려 있는 게임 선택 창이 있다면, 추가로 창을 생성하지 않고 반환
+        if (frame != null) {
+            frame.setState(Frame.NORMAL); // 최소화된 창을 정상 상태로 변경
+            frame.requestFocus(); // 창에 포커스를 줌
+            return;
+        }
+
         // 게임 선택 창을 위한 프레임 생성
-        JFrame frame = new JFrame("게임 선택");
+        frame = new JFrame("게임 선택");
         frame.setLayout(new FlowLayout());
         frame.setSize(350, 100);
-        
-     // 현재 있는 돈 표시
+
+        // 현재 있는 돈 표시
         JLabel moneyLabel = new JLabel("현재 돈: " + money + "원");
         frame.add(moneyLabel);
 
@@ -37,7 +44,7 @@ public class GameStart {
                 new Quiz(); // 퀴즈 게임 시작
             }
         });
-        
+
         // 기억력 게임 버튼
         JButton memoryMatchGameButton = new JButton("기억력 게임");
         memoryMatchGameButton.addActionListener(new ActionListener() {
@@ -46,15 +53,14 @@ public class GameStart {
                 new MemoryMatchGame(); // 기억력 게임 시작
             }
         });
-        
-        
+
         frame.add(rockPaperScissorsButton);
         frame.add(quizButton);
         frame.add(memoryMatchGameButton);
 
         frame.setVisible(true);
     }
-    
+
     public static void increaseMoney(int amount) {
         money += amount;
 
@@ -69,4 +75,3 @@ public class GameStart {
         }
     }
 }
-
