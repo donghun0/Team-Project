c814662d3852d27a9003cad1bdf76494413e6185
LG <LG@10.50.9.78>
2023-12-06 23:27:33 +0900

    최윤하

--- TeamProject/src/game/MemoryMatchGame.java
+++ TeamProject/src/game/MemoryMatchGame.java
@@ -4,156 +4,226 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.Collections;
+import java.util.ArrayList;
 
-public class MemoryMatchGame {
-    private JFrame frame;
-    private List<JButton> cardButtons;
-    private List<String> cardValues;
-    private JButton firstSelectedButton;
-    private String firstSelectedValue;
-    private int matchesFound = 0;
-    private final int timeLimit = 30; // 30초 시간 제한
-    private Timer timer;
+public class MemoryMatchGame extends JFrame implements ActionListener {
+    private ArrayList<Card> cards;
+    private Card selectedCard;
+    private Card c1;
+    private Card c2;
+    private Timer t;
+    private boolean isChecking = false;
+    private int remainingTime = 40;
     private JLabel timeLabel;
-
+    private Timer timer;
+    private boolean gameEnded = false; // 게임 종료 여부를 나타내는 변수 추가
+    
     public MemoryMatchGame() {
-        initializeGame();
-        createUI();
-        startGameTimer();
-    }
-
-    private void initializeGame() {
-        cardValues = new ArrayList<>();
-        for (int i = 1; i <= 8; i++) {
-            String imageName = "image" + i;
-            cardValues.add(imageName);
-            cardValues.add(imageName);
-        }
-        Collections.shuffle(cardValues);
-    }
-
-    private void createUI() {
-        frame = new JFrame("메모리 매칭 게임");
-        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-        frame.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-            	// 창이 닫힐 때 실행될 동작
-            	frame.dispose(); // 현재 프레임을 닫습니다.
-                openGameSelectionWindow(); // 게임 선택 창을 엽니다.
-            }
-        });
-        
-        frame.setLayout(new BorderLayout());
-
-        JPanel gridPanel = new JPanel(new GridLayout(4, 4, 10, 10)); // 4x4 그리드 패널 생성
-        cardButtons = new ArrayList<>();
-        Dimension buttonSize = new Dimension(100, 150);
-        for (int i = 0; i < 16; i++) {
-            JButton cardButton = new JButton();
-            cardButton.setPreferredSize(buttonSize);
-            cardButton.setActionCommand(cardValues.get(i));
-            cardButton.addActionListener(this::cardClicked);
-            cardButtons.add(cardButton);
-            gridPanel.add(cardButton);
-        }
-
-        frame.add(gridPanel, BorderLayout.CENTER); // 그리드 패널을 중앙에 추가
-
-        timeLabel = new JLabel("남은 시간: " + timeLimit, SwingConstants.CENTER);
-        frame.add(timeLabel, BorderLayout.SOUTH); // 타이머 레이블을 하단에 추가
-
-        frame.pack();
-        frame.setVisible(true);
-    }
-
-
-    private void cardClicked(ActionEvent e) {
-        JButton selectedButton = (JButton) e.getSource();
-        String selectedValue = selectedButton.getActionCommand();
-
-        if (firstSelectedButton == selectedButton) {
-            return; // 같은 카드를 두 번 클릭하는 경우 무시
-        }
-
-        // 선택한 카드에 이미지 설정
-        String imagePath = "img/" + selectedValue + ".jpg"; // 'img' 폴더 내의 상대 경로
-        ImageIcon icon = new ImageIcon(imagePath);
-        
-        // 이미지 크기 조절
-        Image scaledImage = icon.getImage().getScaledInstance(100, 150, Image.SCALE_SMOOTH);
-        selectedButton.setIcon(new ImageIcon(scaledImage));
-
-
-        if (firstSelectedButton == null) {
-            firstSelectedButton = selectedButton;
-            firstSelectedValue = selectedValue;
-        } else {
-            if (selectedValue.equals(firstSelectedValue)) {
-                firstSelectedButton.setEnabled(false);
-                selectedButton.setEnabled(false);
-                matchesFound++;
-                firstSelectedButton = null;
-                if (matchesFound == 8) {
-                    timer.stop();
-                    endGame(true);
-                }
-            } else {
-                Timer pause = new Timer(1000, ae -> {
-                    firstSelectedButton.setIcon(null);
-                    selectedButton.setIcon(null);
-                    firstSelectedButton = null;
-                });
-                pause.setRepeats(false);
-                pause.start();
-            }
-        }
-    }
-
-    private void startGameTimer() {
-        timer = new Timer(1000, new ActionListener() {
-            int remainingTime = timeLimit;
-
+        this.cards = new ArrayList<>();
+        this.selectedCard = null;
+        this.t = new Timer(1000, this);
+        this.timer = new Timer(1000, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 remainingTime--;
-                timeLabel.setText("남은 시간: " + remainingTime);
+                timeLabel.setText("남은 시간: " + remainingTime + "초");
+
                 if (remainingTime <= 0) {
                     timer.stop();
-                    endGame(matchesFound == 8);
+                    endGame();
                 }
             }
         });
+
+        setUpCards();
+
+        setTitle("기억력 게임");
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        setSize(400, 400);
+        setLayout(new BorderLayout());
+
+        timeLabel = new JLabel("남은 시간: " + remainingTime + "초");
+        timeLabel.setHorizontalAlignment(JLabel.CENTER);
+        add(timeLabel, BorderLayout.NORTH);
+
+        JPanel cardPanel = new JPanel(new GridLayout(4, 4));
+        for (Card card : cards) {
+            cardPanel.add(card);
+            card.addActionListener(this);
+        }
+        add(cardPanel, BorderLayout.CENTER);
+
+        setVisible(true);
+
         timer.start();
+
+        // 창 닫기 이벤트 처리
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                timer.stop(); // 창 닫을 때 타이머 중지
+                returnToGameStart();
+                dispose();
+            }
+        });
     }
 
+    private void setUpCards() {
+        // 이미지 크기 조정
+        ImageIcon backIcon = new ImageIcon("img/game/back.png");
+        backIcon.setImage(backIcon.getImage().getScaledInstance(95, 95, Image.SCALE_SMOOTH));
 
-    private void endGame(boolean isWin) {
-        String message = isWin ? "성공! 모든 카드를 맞췄습니다!" : "실패! 시간 초과!";
-        
-        // 결과 메시지와 함께 '종료' 버튼을 포함하는 다이얼로그를 생성합니다.
-        Object[] options = {"종료"};
-        int choice = JOptionPane.showOptionDialog(frame, message, "게임 종료", 
-                                                  JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, 
-                                                  null, options, options[0]);
+        for (int i = 1; i <= 8; i++) {
+            ImageIcon frontIcon = new ImageIcon("img/game/image" + i + ".png");
+            // 이미지 크기 조정
+            frontIcon.setImage(frontIcon.getImage().getScaledInstance(95, 95, Image.SCALE_SMOOTH));
 
-        if (choice == 0) {
-            frame.dispose();
-            openGameSelectionWindow();
+            Card c1 = new Card();
+            c1.setId(i);
+            c1.setFrontIcon(frontIcon);
+            c1.setBackIcon(backIcon);
+            c1.setFaceDown();
+
+            Card c2 = new Card();
+            c2.setId(i);
+            c2.setFrontIcon(frontIcon);
+            c2.setBackIcon(backIcon);
+            c2.setFaceDown();
+
+            cards.add(c1);
+            cards.add(c2);
+        }
+
+        Collections.shuffle(cards);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == t) {
+            checkCards();
+        } else if (!isChecking) {
+            Card clickedCard = (Card) e.getSource();
+            if (selectedCard == null && !clickedCard.isMatched() && !clickedCard.isFaceUp()) {
+                selectedCard = clickedCard;
+                selectedCard.flip();
+            } else if (selectedCard != null && clickedCard != selectedCard && !clickedCard.isMatched() && !clickedCard.isFaceUp()) {
+                c1 = selectedCard;
+                c2 = clickedCard;
+                c2.flip();
+
+                isChecking = true;
+                t.start();
+                selectedCard = null;
+            }
         }
     }
-    
-    private void openGameSelectionWindow() {
-        // GameStart 클래스의 메인 메서드를 호출하여 게임 선택 창을 엽니다.
-        GameStart.main(null);
+
+    private void checkCards() {
+        if (c1.getId() != c2.getId()) {
+            c1.flip();
+            c2.flip();
+        } else {
+            c1.setMatched(true);
+            c2.setMatched(true);
+        }
+
+        c1 = null;
+        c2 = null;
+        isChecking = false;
+        t.stop();
+
+        if (checkGameWon()) {
+            endGame();
+        }
+    }
+
+    private boolean checkGameWon() {
+        for (Card card : cards) {
+            if (!card.isMatched()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void endGame() {
+        timer.stop();
+
+        // 게임 종료 여부를 확인하고 한 번만 결과를 표시
+        if (!gameEnded) {
+            gameEnded = true; // 게임 종료 상태로 설정
+
+            if (checkGameWon()) {
+                JOptionPane.showMessageDialog(this, "성공! 모든 카드를 맞췄습니다.");
+            } else {
+                JOptionPane.showMessageDialog(this, "실패! 시간이 초과되었습니다.");
+            }
+
+            returnToGameStart();
+            dispose();
+        }
+    }
+
+    private void returnToGameStart() {
+        GameStart.main(new String[0]);
+        dispose();
     }
 
     public static void main(String[] args) {
-        new MemoryMatchGame();
+        SwingUtilities.invokeLater(() -> new MemoryMatchGame());
+    }
+
+    class Card extends JButton {
+        private int id;
+        private boolean matched = false;
+        private boolean isFaceUp = false;
+        private ImageIcon frontIcon;
+        private ImageIcon backIcon;
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public int getId() {
+            return this.id;
+        }
+
+        public void setFrontIcon(ImageIcon frontIcon) {
+            this.frontIcon = frontIcon;
+        }
+
+        public void setBackIcon(ImageIcon backIcon) {
+            this.backIcon = backIcon;
+        }
+
+        public void flip() {
+            if (isFaceUp) {
+                setIcon(backIcon);
+                isFaceUp = false;
+            } else {
+                setIcon(frontIcon);
+                isFaceUp = true;
+            }
+        }
+
+        public void setFaceDown() {
+            setIcon(backIcon);
+            isFaceUp = false;
+        }
+
+        public boolean isFaceUp() {
+            return isFaceUp;
+        }
+
+        public boolean isMatched() {
+            return matched;
+        }
+
+        public void setMatched(boolean matched) {
+            this.matched = matched;
+        }
     }
 }
