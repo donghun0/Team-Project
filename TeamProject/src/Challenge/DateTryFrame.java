1a5283cadb3115c3fb4c794deaa53f4b36b4e02f
LG <LG@10.50.9.39>
2023-12-09 03:29:31 +0900

    도전

---
+++ TeamProject/src/Challenge/DateTryFrame.java
@@ -0,0 +1,59 @@
+package Challenge;
+
+import javax.swing.*;
+import java.awt.*;
+import shop.Item;
+import shop.Closet;
+
+public class DateTryFrame extends JFrame {
+    private DateTry dateTry;
+    private JLabel scoreLabel;
+    private JLabel feedbackLabel;
+
+    public DateTryFrame(DateTry dateTry) {
+        this.dateTry = dateTry;
+        setSize(500, 500);
+        setLayout(new FlowLayout());
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        scoreLabel = new JLabel();
+        feedbackLabel = new JLabel();
+        
+        System.out.println("DateTryFrame constructor called");
+        updateFeedback();
+
+        add(scoreLabel);
+        add(feedbackLabel);
+
+        setVisible(true);
+        
+        
+    }
+
+    private void updateFeedback() {
+        System.out.println("Updating feedback");
+
+        int score = dateTry.calculateScore();
+        String feedback = dateTry.getFeedback(score);
+        
+        System.out.println("Score: " + score);
+
+
+        scoreLabel.setText("점수: " + score);
+        feedbackLabel.setText("피드백: " + feedback);
+    }
+
+    public static void main(String[] args) {
+        // 여기서 Closet 객체와 DateTry 객체를 생성하고 초기화해야 합니다.
+        Closet closet = new Closet();
+        DateTry dateTry = new DateTry(closet);
+
+        // 예시를 위해 임시로 아이템을 추가합니다.
+        // 실제 구현에서는 사용자의 선택에 따라 아이템이 추가됩니다.
+        closet.addItem(new Item("옷1", 0, "옷"));
+        closet.addItem(new Item("악세서리1", 0, "악세서리"));
+
+        SwingUtilities.invokeLater(() -> new DateTryFrame(dateTry));
+    }
+}
